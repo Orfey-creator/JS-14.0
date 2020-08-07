80461973796596dc397e56eb1b25a57b6c5501d8
Orfey-creator <57431002+Orfey-creator@users.noreply.github.com>
2020-08-07 21:11:04 +0300

    mission comlited

--- lesson01/main.js
+++ lesson01/main.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 20000;
+let income = 'фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 5e6;
+let period = 12;
 
-alert('Мой первый алерт!!!');
+console.log(typeof(money));
+console.log(typeof (income));
+console.log(typeof (deposit));
 
-console.log('Мой первой вывод в консоль!!!');
+console.log(addExpenses.length);
+
+console.log('Период равен '+period);
+console.log('Цель заработать '+mission+' юаней');
+
+addExpenses.toLowerCase();
+console.log(addExpenses.split(','));
+
+const budgetDay = money/=30;
+console.log(budgetDay);
