7e09942bc79d605bd30bb90be070a59680af124a
Orfey-creator <57431002+Orfey-creator@users.noreply.github.com>
2020-08-26 11:30:13 +0300

    fix: create totalValue array for 6 inputs

--- budget__app/js/main.js
+++ budget__app/js/main.js
@@ -1,23 +1,28 @@
 'use strict';
-
+//a
 let calculateButton = document.getElementById('start');
 console.log(calculateButton);
-
+//b
 let plusBtnIncome = document.getElementsByTagName('button')[0];
 console.log(plusBtnIncome);
 
 let plusBtnExpenses = document.getElementsByTagName('button')[1];
 console.log(plusBtnExpenses);
-
+//c
 let checkbox = document.querySelector('#deposit-check');
 console.log(checkbox);
-
+//d
 let additional_income = document.querySelectorAll('.additional_income-item');
 console.log(additional_income);
-
+//e
 let value = document.getElementsByClassName('result-total');
-console.log(value);
 
+let totalValue = [];
+for (let i = 1; i < value.length; i++) {
+  totalValue[i-1] = value[i];
+}
+console.log(totalValue);
+//f
 let salaryAmount = document.querySelector('.salary-amount');
 console.log(salaryAmount);
 let incomeTitle = document.querySelector('.income-title');
